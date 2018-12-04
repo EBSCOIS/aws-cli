225badd2d8eaf857e25b58d44b89e65f4da1eaf7
Brian McAllister <bmcallister@ebsco.com>
2018-12-04 14:57:09 -0500

    Remove tab/spacing issue

--- awscli/customizations/eks/update_kubeconfig.py
+++ awscli/customizations/eks/update_kubeconfig.py
@@ -332,11 +332,10 @@ class EKSClient(object):
                 "-r",
                 self._role_arn
             ])
-	
-	if self._session.profile:
+        
+        if self._session.profile:
             generated_user["user"]["exec"]["env"] = [OrderedDict([
                 ("name", "AWS_PROFILE"),
                 ("value", self._session.profile)
             ])]
-
         return generated_user
